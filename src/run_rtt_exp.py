566aed9e2cf9f3f071a2b23234132b51a5757011
joausaga <jorgesaldivar@gmail.com>
2026-03-06 14:05:28 -0300

    Remove unused imports and modify the huggingface pipeline parameters

--- src/run_rtt_exp.py
+++ src/run_rtt_exp.py
@@ -4,7 +4,7 @@ import os
 
 from dotenv import load_dotenv
 from huggingface_hub import login
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import pipeline
 from tqdm import tqdm
 
 load_dotenv()
@@ -51,17 +51,10 @@ def get_text_prompt(text, from_lang, to_lang):
 
 def do_translation(model_id, sys_prompt, task_prompt, text_prompt):
     
-    if torch.cuda.is_available():
-        device = 'cuda'
-    elif torch.backends.mps.is_available():
-        device = 'mps'
-    else:
-        device = 'cpu'
-    
     pipe = pipeline(
         'text-generation',
         model=model_id,
-        device=device,
+        device='cuda' if torch.cuda.is_available() else 'cpu',
         torch_dtype=torch.bfloat16,
     )
     
