888d635a9ba7b4df7955334da901924adf76afea
Jorge Saldivar <jorgesaldivar@gmail.com>
2026-03-09 22:52:48 +0000

    Improve functions that perform translation

--- src/run_rtt_exp.py
+++ src/run_rtt_exp.py
@@ -4,10 +4,18 @@ import os
 
 from dotenv import load_dotenv
 from huggingface_hub import login
-from transformers import pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.utils import logging
 from tqdm import tqdm
 
+
+# disable logging from transformers to avoid cluttering the output with warnings
+# logging.set_verbosity_error()
+# logging.disable_progress_bar()
+
+
 load_dotenv()
+
 
 def read_experiment_config(config_path):
     with open(config_path, 'r') as f:
@@ -32,14 +40,13 @@ def get_system_prompt(to_lang='guaraní', to_lang_iso='gn',
     """
 
 
-def get_task_prompt(from_lang='español', to_lang='guaraní'):
+def get_task_prompt(text, from_lang='español', to_lang='guaraní'):
     return f"""
-        Tu tarea es traducir el siguiente texto del {from_lang} al {to_lang}, 
-        manteniendo el significado y el estilo del texto original. Asegúrate que 
-        la traducción sea precisa y refleje el tono del texto original. Como 
-        resultado, proporciona solo la traducción al {to_lang} sin ninguna 
-        explicación, comentario, o texto adicional. Tampoco repitas estas 
-        instrucciones en la salida.
+        Traduce el siguiente texto del {from_lang} al {to_lang}, manteniendo 
+        el significado del texto original. El resultado debe ser solo la 
+        traduccion.
+        
+        Texto a traducir: `{text}`
     """.strip()
 
 
@@ -49,45 +56,93 @@ def get_text_prompt(text, from_lang, to_lang):
     """.strip()
 
 
-def do_translation(model_id, sys_prompt, task_prompt, text_prompt):
-    
-    pipe = pipeline(
-        'text-generation',
-        model=model_id,
-        device='cuda' if torch.cuda.is_available() else 'cpu',
-        torch_dtype=torch.bfloat16,
+def load_model(model_id):
+    # load model and tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        dtype=torch.bfloat16,
+        device_map='auto'
     )
-    
-    #messages = [
-    #    {'role': 'system', 'content': 'Eres un experto traductor de idiomas. Tu tarea es traducir el texto que se te proporciona de español a guarani, manteniendo el significado y el estilo del texto original. Proporciona solo la traducción sin ninguna explicación, comentario, o texto adicional.'},
-    #    {'role': 'user', 'content': 'El festival de cannes se celebrara en mayo proximo'},
-    #]
-    
-    #prompt = tokenizer.apply_chat_template(
-    #    messages,
-    #    tokenize=False,
-    #    add_generation_prompt=True
-    #)
-    
-    prompt = """
-        Traduce el siguiente texto del español al guaraní.
-        Responde únicamente con la traducción.
+    # put model in evaluation mode (inference)
+    model.eval()
+    return model, tokenizer
 
-        Texto a traducir: `El festival de Cannes se celebrará en mayo próximo`
-    """
+
+def do_translation(model, tokenizer, sys_prompt, task_prompt):
+    messages = [
+        {'role': 'system', 'content': sys_prompt},
+        {'role': 'user', 'content': task_prompt},
+    ]
+    # apply the chat template
+    prompt = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    # apply tokenization and move the input tensors to the same device as the model
+    inputs = tokenizer(
+        prompt, return_tensors='pt', padding=True, truncation=True
+    ).to(model.device)
+    # generate translation
+    with torch.no_grad():
+        output = model.generate(
+            **inputs,
+            max_new_tokens=200
+        )
+    #translated_text = tokenizer.batch_decode(output, skip_special_tokens=True)
+    translated_text = tokenizer.decode(
+        output[0][inputs['input_ids'].shape[-1]:],
+        skip_special_tokens=True
+    )
+    return translated_text
+
+
+def do_batch_translation(model, tokenizer, sys_prompt, task_prompt, sentences,
+                         batch_size=16):
     
-    output = pipe(prompt, max_new_tokens=200)
+    trans_results = []
+    content_prompt = task_prompt + '\n\n'
+    for i in tqdm(range(0, len(sentences), batch_size), desc='Translating sentences in batches'):
+        batch = sentences[i:i + batch_size]
+        for sentence in batch:
+            # concatenate batch sentence to the task prompt
+            content_prompt += f'`{sentence}`\n'    
+        messages = [
+            {'role': 'system', 'content': sys_prompt},
+            {'role': 'user', 'content': content_prompt},
+        ]
+        # apply the chat template
+        prompt = tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+        # apply tokenization and move the input tensors to the same device as the model
+        inputs = tokenizer(
+            prompt, return_tensors='pt', padding=True, truncation=True
+        ).to(model.device)
+        # generate translation
+        with torch.no_grad():
+            outputs = model.generate(
+                **inputs,
+                max_new_tokens=200
+            )
+        # process output
+        decoded = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        trans_results.append(decoded[len(prompt):].strip())
     
-    print(output[0]['generated_text'][len(prompt):]) 
+    return trans_results
     
 
 def run_rtt(rtt_data, list_base_models, output_dir, to_lang='guaraní', to_lang_iso='gn', 
             from_lang='español', from_lang_iso='es'):
     sys_prompt = get_system_prompt(to_lang, to_lang_iso, from_lang, from_lang_iso)
     results = []
-    for base_model in tqdm(list_base_models, desc=f'Running RTT for models'):
+    for base_model in tqdm(list_base_models, desc=f'Running RTT'):
         for model_variant in base_model['variants']:
             model_variant_name = model_variant['huggingface_id'].split('/')[-1].lower()
+            model_variant_id = model_variant['huggingface_id']
             if model_variant_name == 'gemma-3-4b-it':
                 rtt_model = {
                     'model': {
@@ -99,19 +154,19 @@ def run_rtt(rtt_data, list_base_models, output_dir, to_lang='guaraní', to_lang_
                     },
                     'results': []
                 }
+                model, tokenizer = load_model(model_variant_id)
                 for record in tqdm(rtt_data, desc=f'Translating sentences with {model_variant_name}'):
                     source_text = record['text']
-                    # translate to guaraní using the base model (not implemented here)
-                    task_prompt = get_task_prompt(from_lang, to_lang)
-                    text_prompt = get_text_prompt(source_text, from_lang, to_lang)
+                    # translate to `to_lang` using the model
+                    task_prompt = get_task_prompt(source_text, from_lang, to_lang)
                     trans_text_to_lang = do_translation(
-                        model_variant['huggingface_id'], sys_prompt, task_prompt, text_prompt
+                        model, tokenizer, sys_prompt, task_prompt
                     )
-                    # translate back to spanish using the base model (not implemented here)
-                    task_prompt = get_task_prompt(to_lang, from_lang)
-                    text_prompt = get_text_prompt(trans_text_to_lang, to_lang, from_lang)
+                    trans_text_to_lang = trans_text_to_lang.replace('\n', ' ').strip()
+                    # translate back to `from_lang` using the model
+                    task_prompt = get_task_prompt(trans_text_to_lang, to_lang, from_lang)
                     trans_text_from_lang = do_translation(
-                        model_variant['huggingface_id'], sys_prompt, task_prompt, text_prompt
+                        model, tokenizer, sys_prompt, task_prompt
                     )
                     rtt_model['results'].append(
                         {
