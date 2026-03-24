8c8fef4ac445b174ee2d54268c19ac2faa180ce9
joausaga <jorgesaldivar@gmail.com>
2026-03-24 13:35:58 -0300

    Fix a bug in the instruction that obtain the model name

--- src/eval_rrt_exp.py
+++ src/eval_rrt_exp.py
@@ -9,7 +9,7 @@ from sacrebleu.metrics.chrf import CHRF
 from utils.utils import clean_text
 from utils.utils import get_random_text
 from utils.utils import tokenize
-from utils.utils import read_bench_data
+from utils.utils import read_jsonl
 from corpus.src.pipeline.language_identifier.language_identifier import LanguageIdentifier
 from tqdm import tqdm
 
@@ -111,7 +111,8 @@ def run_evaluation(result_file_path, from_lang, to_lang, metrics, bench_data):
         new_model_results['rtt_translation'].append(translation_dict)
     # run overall evaluation
     print('Conducting overall evaluation...')
-    overall_eval = {'model_name': model_results['model']['name'].split('/')[1]}
+    model_name = model_results['model']['name'].split('/')[1] if '/' in model_results['model']['name'] else model_results['model']['name']
+    overall_eval = {'model_name': model_name}
     for metric in metrics:
         eval_result = evaluate_results(predictions, references, metric, 'corpus')
         if eval_result:
@@ -151,7 +152,7 @@ def get_bench_data(project_dir, lang):
         bench_dataset_file_path = os.path.join(project_dir, 'data', 'RTTBench-Mono-ES.jsonl')
     else:
         bench_dataset_file_path = os.path.join(project_dir, 'data', 'RTTBench-Mono.jsonl')
-    data = read_bench_data(bench_dataset_file_path)
+    data = read_jsonl(bench_dataset_file_path)
     bench_data = {d['id']: d['domain'] for d in data}
     return bench_data
  
